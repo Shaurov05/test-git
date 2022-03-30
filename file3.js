3521c9eef71abd7b3418384960bb2e63b0b316ee
Shaurov05 <sacshaurov05@gmail.com>
2022-03-30 16:44:12 +0600

    new changes

--- file3.js
+++ file3.js
@@ -8,3 +8,6 @@ console.log("update from file 3")
 console.log("update from file 3")
 
 console.log("update from file 3 new")
+
+console.log("update from file 3 new")
+console.log("update from file 3 new")
