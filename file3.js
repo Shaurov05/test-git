ea85a6d058b1a35f09992231d8a9d1ea43cd9dd0
Shaurov05 <sacshaurov05@gmail.com>
2022-03-30 18:05:01 +0600

    change from sayad 1

--- file3.js
+++ file3.js
@@ -1,16 +1,7 @@
 console.log("this is file 3")
 
 console.log("update from file 3")
-console.log("update from file 3")
-console.log("update from file 3")
-console.log("update from file 3")
-console.log("update from file 3")
-console.log("update from file 3")
 
 console.log("update from file 3 new")
 
-console.log("update from file 3 new")
-console.log("update from file 3 new")
-
-console.log("update from file 4 new")
 console.log("update from file 4 new")
